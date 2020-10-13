1bfdfef494a5a8b59e49a548cda89e3b9854ea10
kumarangp <30560097+kumarangp@users.noreply.github.com>
2020-10-13 13:14:39 +0530

    Upto video 41

--- Core/Specifications/ProductsWithTypesandBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesandBrandsSpecification.cs
@@ -6,12 +6,13 @@ namespace Core.Specifications
 {
     public class ProductsWithTypesandBrandsSpecification : BaseSpecification<Product>
     {
+        // Get all product.
         public ProductsWithTypesandBrandsSpecification()
         {
             AddInclude(x => x.ProductType);
             AddInclude(x => x.ProductBrand);
         }
-
+        // Get single product.
         public ProductsWithTypesandBrandsSpecification(int id)
             : base(x => x.Id == id)
         {
